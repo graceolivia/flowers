6aa6d6913fa95c91af7fdf4b879bdc2ee5e35802
grace <gracelidinskysmith@gmail.com>
2023-05-16 12:56:39 -0400

    fix: ADD CSS FROM TUTORIAL

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,21 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
 export default function IndexRoute() {
-    return <div>Gorgeous flowers for sale</div>;
+  return (
+    <div>
+      <h1>Helo</h1>
+
+      <p>Gorgeous flowers for sale</p>
+      <li>
+        <Link to="flowers">Find flowers!????</Link>
+      </li>
+    </div>
+  );
 }
--- app/routes/flowers.tsx
+++ app/routes/flowers.tsx
@@ -1,13 +1,21 @@
-import {LiveReload, Outlet} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { Link, LiveReload, Outlet } from "@remix-run/react";
+
+import stylesUrl from "~/styles/flowers.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
 
 export default function JokesRoute() {
-    return (
-        <div>
-            <h1>FL🌸WERS</h1>
-            <main>
-                <Outlet />
-                <LiveReload />
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <h1>FL🌸WERS</h1>
+      <h2>hi</h2>
+      <main>
+        <Outlet />
+        <LiveReload />
+      </main>
+    </div>
+  );
 }
