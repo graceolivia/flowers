f57697e0b8fe371910bb52b7b6074847b1f100db
grace <gracelidinskysmith@gmail.com>
2023-05-17 14:55:30 -0400

    add data validation

--- app/routes/flowers.new.tsx
+++ app/routes/flowers.new.tsx
@@ -1,7 +1,20 @@
 import type { ActionArgs } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
+function validateFlowerContent(content: string) {
+  if (content.length > 1) {
+    return "One character only please";
+  }
+}
+
+function validateFlowerName(name: string) {
+  if (name.length < 2) {
+    return "That flower's name is too short";
+  }
+}
 
 export const action = async ({ request }: ActionArgs) => {
   const form = await request.formData();
@@ -10,31 +23,73 @@ export const action = async ({ request }: ActionArgs) => {
   // we do this type check to be extra sure and to make TypeScript happy
   // we'll explore validation next!
   if (typeof content !== "string" || typeof name !== "string") {
-    throw new Error("Form not submitted correctly.");
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
   }
-
+  const fieldErrors = {
+    content: validateFlowerContent(content),
+    name: validateFlowerName(name),
+  };
   const fields = { content, name };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
 
   const flower = await db.flower.create({ data: fields });
   return redirect(`/flowers/${flower.id}`);
 };
 
 export default function NewJokeRoute() {
+  const actionData = useActionData<typeof action>();
   return (
     <div>
       <p>Add a flower to the bouquet</p>
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name:{" "}
+            <input
+              defaultValue={actionData?.fields?.name}
+              name="name"
+              type="text"
+              aria-invalid={Boolean(actionData?.fieldErrors?.name)}
+              aria-errormessage={
+                actionData?.fieldErrors?.name ? "name-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" id="name-error" role="alert">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
         <div>
           <label>
-            Content (Emoji please): <textarea name="content" />
+            Content (Emoji please):{" "}
+            <textarea
+              defaultValue={actionData?.fields?.content}
+              name="content"
+              aria-invalid={Boolean(actionData?.fieldErrors?.content)}
+              aria-errormessage={
+                actionData?.fieldErrors?.content ? "content-error" : undefined
+              }
+            />
           </label>
         </div>
         <div>
+          {actionData?.formError ? (
+            <p className="form-validation-error" role="alert">
+              {actionData.formError}
+            </p>
+          ) : null}
           <button type="submit" className="button">
             Add
           </button>
